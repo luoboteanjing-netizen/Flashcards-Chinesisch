36fe1c9b6d6d6c3bf06fdbfafc8b8cbc251f030a
luoboteanjing-netizen <luobote.anjing@gmail.com>
2026-04-02 23:02:00 +0200

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,7 +22,8 @@ const csvParam = params.get("csv");
 // Sonst → Standarddatei verwenden
 const CSV_URL = csvParam 
     ? `./data/${csvParam}`
-    : "./data/Long-Chinesisch_Lektionen.csv";
+    : "./data/HSK-Chinesisch_Lektionen.csv",
+	  "./data/Long-Chinesisch_Lektionen.csv";
 
 const LS_KEYS = {
     settings: "fc_settings_v1",
